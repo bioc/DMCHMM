dfd1d40758689c94a43f52e897208e08836ccb9a
Farhad Shokoohi <shokoohi@icloud.com>
2021-09-01 18:13:38 -0700

    ver10151

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -47,7 +47,7 @@
 #' @param nsamp An integer value
 #' @param formula A formula
 #' @param FDRthreshold A numeric value
-#' @param Methylthreshold A numeric value
+#' @param Methylthreshold A positive numeric value; the default is 0.001
 #' @param weightfunction A function to create weights using variance obtained
 #' form the MCMC algorithm
 #' @param ... other possible parameters
@@ -68,6 +68,8 @@
 #' plot.
 #' @param annotateTop If TRUE, only annotates the top hit on each chromosome
 #' that is below the annotatePval threshold.
+#' @param windowsize An integer value for partitioning data into windows of size
+#' windowsize.
 #' @docType NULL
 NULL
 
